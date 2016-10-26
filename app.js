cbf2e581f1838e61c1698661095a6bb7dc59003d
avmadhukiran <avmadhukiran@gmail.com>
2016-10-27 00:56:49 +0530

    hi

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var app = express();
 var port=Number(process.env.PORT || 3000);
 
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('public/index.html' , { root : __dirname});
+  console.log("hie");
 });
+
+
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
